806c752a3f1e1f7ff325bea5a3600a66bd321ef7
chenxiemin <chenxiemin@gmail.com>
2014-11-17 17:28:49 +0800

    update color

--- Player/image-merger.h
+++ Player/image-merger.h
@@ -24,7 +24,7 @@ class ImageMerger
 		int picHeight = outPicture->GetHeight();
 		int picWidth = outPicture->GetWidth();
 
-		memset(pic.data[0], 100, pic.linesize[0] * picHeight);
+		memset(pic.data[0], 0, pic.linesize[0] * picHeight);
 		memset(pic.data[1], 0x80, pic.linesize[1] * picHeight / 2);
 		memset(pic.data[2], 0x80, pic.linesize[1] * picHeight / 2);
 
@@ -50,6 +50,35 @@ class ImageMerger
 				int scalej = (int)(((double)frameHeight / perHeight) * perj);
 				int scalei = (int)(((double)frameWidth / perWidth) * peri);
 				pic.data[0][j * picWidth + i] = curFrame->GetData()[0][scalej * curFrame->GetLinesize()[0] + scalei];
+				// pic.data[2][j * picWidth / 4 + i / 2] = curFrame->GetData()[2][scalej / 2 * curFrame->GetLinesize()[2] + scalei / 2];
+			}
+		}
+
+		perWidth /= 2;
+		perHeight /= 2;
+		picWidth /= 2;
+		picHeight /= 2;
+		for (int j = 0; j < picHeight; j++) {
+			for (int i = 0; i < picWidth; i++) {
+				// get the row and column
+				int colIndex = j / perHeight;
+				int rowIndex = i / perWidth;
+				// get associate frame
+				std::shared_ptr<MyAVFrame> curFrame = inFrames[colIndex * mrow + rowIndex];
+				if (NULL == curFrame.get())
+					continue;
+
+				// map color
+				int perj = j % perHeight; // per block index
+				int peri = i % perWidth;
+
+				int frameWidth = curFrame->GetWidth() / 2;
+				int frameHeight = curFrame->GetHeight() / 2;
+				int scalej = (int)(((double)frameHeight / perHeight) * perj);
+				int scalei = (int)(((double)frameWidth / perWidth) * peri);
+
+				pic.data[1][j * picWidth + i] = curFrame->GetData()[1][scalej * curFrame->GetLinesize()[1] + scalei];
+				pic.data[2][j * picWidth + i] = curFrame->GetData()[2][scalej * curFrame->GetLinesize()[2] + scalei];
 			}
 		}
 #else
